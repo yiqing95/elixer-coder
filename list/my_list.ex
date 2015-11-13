9c4389d7c6968f301549714029f32337502f657b
yiqing <yiqing_95@qq.com>
2015-11-14 01:24:07 +0800

    基于原始列表 （数组） 来创建新的列表 （数组）

--- list/my_list.ex
+++ list/my_list.ex
@@ -5,6 +5,13 @@ defmodule MyList do
 
   def len([_head | tail ]) , do: 1 + len(tail)
 
+  #  对某个整数的列表 返回一个新的列表 元素是原先响应位上的平方
+  def square([]) , do: []
+  #  递归
+  def square([head|tail]) , do: [ head * head | square( tail ) ]
 
 
+  # 对列表中的每个元素自身增1
+  def add_1( [] ) , do: []
+  def add_1([ head | tail ]) , do: [ head+1 | add_1(tail) ]
 end
