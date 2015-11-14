eefd2d45c91e0aa98ed00b86e5e58aa1cdca6567
yiqing <yiqing_95@qq.com>
2015-11-14 10:22:00 +0800

    练习

--- list/my_list.ex
+++ list/my_list.ex
@@ -27,5 +27,39 @@ defmodule MyList do
   defp _sum( [] , total ) , do: total
   defp _sum( [ head | tail ] , total) , do: _sum(tail , head + total )
 
+  # Reduce 函数 效果类似百叶窗的折叠
+  def reduce([] ,value , _ ) do
+    value
+  end
 
+  def reduce([head | tail ] , value ,func ) do
+    reduce(tail ,func.(head,value) , func)
+  end
+
+
+  ##  练习
+  def mapsum([] ,_ ) , do: 0
+  def mapsum([ h | t ],func) do
+      _mapsum([h|t], func , 0)
+  end
+
+   def _mapsum([] ,_ ,val) , do: val
+    def _mapsum([ h | t ],func, val) do
+         _mapsum(t,func, func.(h) + val)
+    end
+
+    ##  求最大值
+    def max([]) , do:  0
+    def max(l) , do: _max(l,0)
+    def _max([] , val), do: val
+    def _max([h |  t] , val) when h > val do
+        _max(t,h)
+    end
+    def _max([h |  t] , val) when h < val do
+            _max(t,val)
+    end
+
+    # TODO 搞不定了！
+    # def caesar([] , _ ) , do: []
+    # def caesar([h | t] , n) , do: ( [ n+h | caesar(t , n )] )
 end
