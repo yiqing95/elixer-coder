e13ee1fb72c7461d4d85a99407debf227d734ae3
yiqing <yiqing_95@qq.com>
2016-02-12 08:19:13 +0800

    第一个简单的OTP server例子

--- learning-elixir/7/codes/k_v.ex
+++ learning-elixir/7/codes/k_v.ex
@@ -11,5 +11,12 @@ defmodule KV do
     {:ok, HashDict.new}
   end
 
+  ## 处理消息
+ def handle_call({:put, key, value}, _from, dictionary) do
+ {:reply, :ok, HashDict.put(dictionary, key, value)}
+ end
+ def handle_call({:get, key}, _from, dictionary) do
+ {:reply, HashDict.get(dictionary, key), dictionary}
+ end
 
 end
