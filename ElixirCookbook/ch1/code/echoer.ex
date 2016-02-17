66ac856a9240d85b1e2eb51994754b1e4a8e92b7
yiqing <yiqing_95@qq.com>
2016-02-17 08:31:58 +0800

    cookbook 学习

---
+++ ElixirCookbook/ch1/code/echoer.ex
@@ -0,0 +1,5 @@
+defmodule Echoer do
+  def echo(msg) do
+    IO.puts "#{msg} ... #{msg} ...... #{msg}" 
+  end
+end
---
+++ ElixirCookbook/ch1/code/greeter.ex
@@ -0,0 +1,6 @@
+defmodule Greater do
+
+  def great(name \\ "you") do
+    "Hello #{name} !"
+  end
+end
